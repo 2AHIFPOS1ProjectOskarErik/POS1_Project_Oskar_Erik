4c8b8258110cbe762c7c7514a188640a1ef7994f
Oskar Blaser <laser.blaser09@gmail.com>
2026-06-01 23:20:32 +0200

    Minibossraum hinzugefügt, sobald man den Raum betritt wird der Eingang verschlossen, Miniboss existiert macht aber noch nichts

---
+++ Assets/Scripts/Enemies/MiniBoss.cs
@@ -0,0 +1,84 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MiniBoss : MonoBehaviour
+{
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    Rigidbody2D rb;
+    BoxCollider2D collider2D;
+    float direction;
+    float timer = 0f;
+    public double hp = 5;
+    public GameObject miniBoss;
+    public GameObject player;
+    private Movement playercode;
+
+    public int moneyReward = 10;
+
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(miniBoss);
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        player = GameObject.Find("Player");
+        playercode = player.GetComponent<Movement>();
+    }
+
+
+    private void OnDestroy()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+
+    void Start()
+    {
+        direction = 0f;
+        rb = GetComponent<Rigidbody2D>();
+        collider2D = GetComponent<BoxCollider2D>();
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.linearVelocity = new Vector2(direction * 5f, rb.linearVelocityY);
+        timer += Time.deltaTime;
+        if (hp <= 0)
+        {
+            miniBoss.SetActive(false);
+            Geld_anzeige.Instance.AddMoney(10);
+
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Wall"))
+        {
+            if (direction > 0f)
+            {
+                direction = direction * (-1);
+            }
+            else if (direction < 0f)
+            {
+                direction = direction * (-1);
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+
+        if (other.CompareTag("Weapon 1"))
+        {
+            hp -= playercode.dmg;
+            rb.linearVelocity = new Vector2(8f, 5f);
+        }
+    }
+
+}
